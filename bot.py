d3217a762e6ee50aeab545611e291e012e867f75
DenK0 <denidinamo@gmail.com>
2023-12-11 01:36:16 +0200

    bot_done?)

--- bot.py
+++ bot.py
@@ -10,59 +10,84 @@ def input_error(func):
             return "Invalid command. Please try again."
     return wrapper
 
+
 contacts = {}
+
+
+@input_error
+def hello():
+    return "How can I help you?"
+
 
 @input_error
 def add_contact(name, phone):
+    if name in contacts:
+        return "Contact already exists. Use the 'change' command to modify the phone number."
     contacts[name] = phone
     return "Contact added successfully"
 
+
 @input_error
-def change_phone(name, phone):
+def change_contact(name, phone):
+    if name not in contacts:
+        return "Contact not found. Use the 'add' command to add a new contact."
     contacts[name] = phone
     return "Phone number updated successfully"
 
+
 @input_error
-def get_phone(name):
+def phone_contact(name):
     return contacts[name]
 
+
+@input_error
 def show_all_contacts():
     if not contacts:
         return "No contacts found"
     else:
         return "\n".join(f"{name}: {phone}" for name, phone in contacts.items())
 
+
+@input_error
+def good_bye():
+    return "Good bye!"
+
+
+COMMAND = {
+    "hello": hello,
+    "add": add_contact,
+    "change": change_contact,
+    "phone": phone_contact,
+    "show_all": show_all_contacts,
+    "exit": good_bye
+}
+
+
+@input_error
+def process_command(command):
+    command_parts = command.split(maxsplit=1)
+    cmd = command_parts[0].lower()
+    args = command_parts[1] if len(command_parts) > 1 else None
+    func = COMMAND.get(cmd)
+    if func:
+        if args:
+            print(func(*args.split()))
+        else:
+            print(func())
+    else:
+        print("Invalid command. Please try again.")
+
+
+@input_error
 def main():
     while True:
-        command = input("Enter a command: ").lower()
-
-        if command == "hello":
-            print("How can I help you?")
-        elif command.startswith("add"):
-            try:
-                _, name, phone = command.split()
-                print(add_contact(name, phone))
-            except ValueError:
-                print("Invalid input. Please enter name and phone number separated by a space.")
-        elif command.startswith("change"):
-            try:
-                _, name, phone = command.split()
-                print(change_phone(name, phone))
-            except ValueError:
-                print("Invalid input. Please enter name and phone number separated by a space.")
-        elif command.startswith("phone"):
-            try:
-                _, name = command.split()
-                print(get_phone(name))
-            except ValueError:
-                print("Invalid input. Please enter a name.")
-        elif command == "show all":
-            print(show_all_contacts())
-        elif command in ["good bye", "close", "exit"]:
-            print("Good bye!")
+        user_input = input("Enter command: ").lower()
+        if user_input in ("good bye", "close", "exit"):
+            print(COMMAND["exit"]())
             break
         else:
-            print("Invalid command. Please try again.")
+            process_command(user_input)
+
 
 if __name__ == "__main__":
     main()
